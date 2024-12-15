ab295c485d239304d6150bb016e7dfe8a05cc937
veissa <cashmer552@gmail.com>
2024-12-15 14:23:40 +0100

    Add files via upload

---
+++ Python/warmup-1/front_back.py
@@ -0,0 +1,10 @@
+# Given a string, return a new string where the first and last chars have been exchanged.
+
+# front_back('code') → 'eodc'
+# front_back('a') → 'a'
+# front_back('ab') → 'ba'
+
+def front_back(str):
+  if len(str)<=1:
+    return str
+  return str[len(str)-1] + str[1:len(str)-1] + str[0]
