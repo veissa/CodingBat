2e3e8a5d033f73888db3d68b64a47ff4f93f358f
Carthica <cashmer552@gmail.com>
2024-12-16 22:55:18 +0100

    Create delDel.java

---
+++ Java/delDel.java
@@ -0,0 +1,8 @@
+public String delDel(String str) {
+  if(str.length()>=4){
+    if(str.substring(1,4).equals("del")){
+      return str.substring(0,1)+str.substring(4);
+    }
+  }
+  return str;
+}
